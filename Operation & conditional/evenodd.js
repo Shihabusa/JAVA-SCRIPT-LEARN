eef8bbbc59319e155a7fd985aa1b3b54d21cac6e
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-10 11:19:27 +0600

    evenodd added

--- Operation & conditional/evenodd.js
+++ Operation & conditional/evenodd.js
@@ -1,7 +1,30 @@
-let num =10;
-if (num%2==0){
-    console.log("even")
-}
-else{
-    console.log("odd")
-}
+// let num =10;
+// if (num%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+// else if 
+// let mode="dark";
+// let color;
+
+// if(mode==="dark"){
+//     color="black";
+// }
+// else if(mode==="blue"){
+//     color="blue";
+// }
+// else if(mode==="pink"){
+//     color="pink";
+// }
+// else{
+//     color="white";
+// }
+// console.log(color);
+
+//ternary operator 
+let age =25
+let result= age>18? "Adult": "not adult";
+console.log(result);
