3ce4e98d1fe06229164ff27c9ba48b8cf12c83d9
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-17 01:27:24 +0600

    Dom is practicing 25.8 completed.

--- Tour of Dom/style.js
+++ Tour of Dom/style.js
@@ -2,4 +2,14 @@ const sections=document.querySelectorAll('section')
 //console.log(section)
 for(const section of sections){
     section.style.border='2px solid black'
+    section.style.margin="3px"
+    section.style.borderRadius='10px'
+    section.style.padding='20px'
+    section.style.backgroundColor='lightgreen'
 }
+const placecontain=document.getElementById('places')
+placecontain.style.backgroundColor='lightgrey'
+const lifeclass=document.getElementById('lifes')
+
+lifeclass.classList.add('big-text')//make a style in css and added that using js 
+lifeclass.classList.remove('text-all')//kono class er css er kaj remove kora
