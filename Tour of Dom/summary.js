f75460a8ab53165c7ebea162a805ac370165c3da
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-17 21:08:25 +0600

    25 is complete

---
+++ Tour of Dom/summary.js
@@ -0,0 +1,13 @@
+// console.log('hellow java')
+// console.log(document)
+const allli=document.getElementsByTagName('li')
+console.log(allli)
+
+const alltitle=document.getElementsByClassName('title')
+console.log(alltitle)
+
+const sec=document.getElementById('Second')
+console.log(sec)
+sec.style.color='red'
+sec.style.backgroundColor='black'
+//document.querySelectorAll
---
+++ Tour of Dom/summary2.js
@@ -0,0 +1,7 @@
+console.log('Second File');
+const secondlist=document.getElementById('sec-list')
+const li=document.createElement('li')
+li.innerText='my dynamic item'
+secondlist.appendChild(li)//akn add hoise
+
+//main 
