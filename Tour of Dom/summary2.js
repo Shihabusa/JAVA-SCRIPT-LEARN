d98e8cfa15371bf3dc4fe79068d3575f3c48d292
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-17 21:21:39 +0600

    25 is complete

--- Tour of Dom/summary2.js
+++ Tour of Dom/summary2.js
@@ -1,7 +1,23 @@
-console.log('Second File');
-const secondlist=document.getElementById('sec-list')
-const li=document.createElement('li')
-li.innerText='my dynamic item'
-secondlist.appendChild(li)//akn add hoise
+// const { createElement } = require("react");
 
-//main 
+// console.log('Second File');
+// const secondlist=document.getElementById('sec-list')
+// const li=document.createElement('li')
+// li.innerText='my dynamic item'
+// secondlist.appendChild(li)//akn add hoise
+
+//main e korte gele ager sob comment out koro or new file theke koro
+const main=document.getElementById('container')
+const section=document.createElement('section')
+section.innerHTML=`
+<h1>my dynamic created section</h1>
+<p>Extra tag is added inside paragraph</p>
+<ul>
+<li>My first </li>
+<li>My second</li>
+<li>My third </li>
+<li>My last </li>
+</ul>
+`;
+main.appendChild(section);
+
