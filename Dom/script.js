b446d5d354242be0a3cf9edc8002d593433ca717
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-03-27 22:18:34 +0600

    problem of dom

--- Dom/script.js
+++ Dom/script.js
@@ -1,2 +1,15 @@
-alert("Hello beautiful!")
+// alert("Hello beautiful!")
 //console.dir(document)
+// let div=document.querySelector('div')
+// console.log(div)
+// let id=div.getAttribute('id')
+// console.log(id);
+// let name=div.setAttribute('name','abcd')
+// console.log(name);
+
+//dom 2
+let div=document.querySelector('div')
+console.log(div)
+div.style.backgroundColor='red';
+div.style.fontSize='28px'
+div.innerText="hellow";
