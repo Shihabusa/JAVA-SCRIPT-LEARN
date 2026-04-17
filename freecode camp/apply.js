c5957f654c2fcff290f7b8230af844f5a211eb91
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-04-17 09:38:04 +0600

    Dom learning from free code camp

---
+++ freecode camp/apply.js
@@ -0,0 +1,42 @@
+
+
+//GetElementById()
+// const title = document.getElementById('title');
+// console.log(title);
+
+// const title = document.getElementById('main-heading');
+// console.log (title);
+
+
+
+//GetElementsByClassName()
+// const items = document.getElementsByClassName('item');
+// console.log(items);
+
+// const movies=document.getElementsByClassName('movie')
+// console.log(movies);
+
+//GetElementsByTagName()
+// const li = document.getElementsByTagName('li');
+// console.log(li);
+
+// const li = document.getElementsByTagName('li')
+// console.log(li);
+
+//QuerySelector()
+// const header = document.querySelector('#main-header');
+// console.log(header);
+
+// const container = document.querySelector('div');//single element selector
+// console.log(container);
+
+
+//QuerySelectorAll()
+// const titles = document.querySelectorAll('.title');//double element selector
+// console.log(titles);
+
+// const containers = document.querySelectorAll('div');
+// console.log(containers);
+
+//DOM manipulation 
+
---
+++ freecode camp/apply1.js
@@ -0,0 +1,20 @@
+//Dom manipulation
+
+//styling elements using DOM
+// const title=document.querySelector('#main-heading')
+// //access css property
+// title.style.color="blue"//inline styling
+// console.log(title);
+
+// const listItems=document.querySelectorAll('.movie');
+// // listItems.forEach(function(item){
+// //     item.style.fontSize="20px";
+// //     item.style.color="red";
+// // });
+// for(i=0;i<listItems.length;i++){
+//     listItems[i].style.fontSize="50px";
+// }
+
+// console.log(listItems);
+
+//
