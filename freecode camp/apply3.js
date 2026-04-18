588491cf1bb6cda3f28480f45dff9d05d27f753b
Abdur Rahaman <ieltsshihab01@gmail.com>
2026-04-18 17:29:50 +0600

    click and mouse over event learn

--- freecode camp/apply3.js
+++ freecode camp/apply3.js
@@ -1,3 +1,21 @@
 //dom manipulation
 
 //Event listener
+
+//element.addEventListener('click',function)
+
+const btntwo=document.querySelector('.btn-2');
+function alrtbtn(){
+    alert("I also love JavaScript");
+
+};
+btntwo.addEventListener('click',alrtbtn);
+
+
+//mouseover event
+
+const btn1BG=document.querySelector('.card-1');
+function cngBG(){
+    btn1BG.style.backgroundColor='blue';
+}
+btn1BG.addEventListener('mouseover',cngBG);
